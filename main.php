4872872ae8c47a4cce86d5e6d94f373a5701b967
Radka <stefanikovaradka@gmail.com>
2014-06-30 13:35:47 +0200

    [css] nove pismo

--- main.php
+++ main.php
@@ -23,7 +23,7 @@ echo '
 
   <link href="http://fonts.googleapis.com/css?family=Shadows+Into+Light+Two&subset=latin,latin-ext" rel="stylesheet" type="text/css">
   <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,300,400italic,400,600italic,600,700italic,700,800italic,800" rel="stylesheet" type="text/css">
-
+  <script src="//use.edgefonts.net/gloria-hallelujah:n4:all;source-sans-pro.js"></script>
   <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
   <title>
 ';
