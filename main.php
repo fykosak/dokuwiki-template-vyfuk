38558426970ac187f772c34f2c7bd0b7f647fe72
Míra Jarý <jason@vyfuk.mff.cuni.cz>
2019-11-02 09:46:58 +0100

    Initial commit of new design

--- main.php
+++ main.php
@@ -1,151 +1,105 @@
-<?php
-if (!defined('DOKU_INC')) {
-    die();
-}
+<?php if (!defined('DOKU_INC')) die();
 
-// include custom template functions stolen from arctic template 
-require_once(dirname(__FILE__) . '/tpl_functions.php');
+global $conf;
+global $ID;
+$orange = "#ff4800";
+
 require_once(dirname(__FILE__) . '/navBar/BootstrapNavBar.php');
 require_once(dirname(__FILE__) . '/navBar/NavBarItem.php');
 
-// Hide php warnings caused mainly by syntax
-error_reporting(E_ALL ^ E_WARNING ^ E_NOTICE);
-
-global $ACT;
-global $ID;
-global $conf;
+//Code extracted from fkstpl for getting pageId
+/*class pageProp {	
+	private $pageId;
+	/**
+	* @param $pageId
+	* @return $this
+	
+	public function setPageId($pageId) {
+		$this->pageId = $pageId;
+		return $this;
+	}
+}*/
 ?>
 
 <!DOCTYPE html>
-
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $conf['lang']; ?>" lang="<?php echo $conf['lang']; ?>"
-      dir="<?php echo $lang['direction'] ?>">
-
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $conf['lang']; ?>" lang="<?php echo $conf['lang']; ?>" dir="<?php echo $lang['direction'] ?>">
 <head>
-    <link href="http://fonts.googleapis.com/css?family=Shadows+Into+Light+Two&amp;subset=latin,latin-ext"
-          rel="stylesheet" type="text/css"/>
-    <link
-            href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,300,400italic,400,600italic,600,700italic,700,800italic,800"
-            rel="stylesheet" type="text/css"/>
-    <script type="text/javascript" src="//use.edgefonts.net/gloria-hallelujah:n4:all;source-sans-pro.js">
-    </script>
-
-
-    <meta name="viewport" content="width=device-width, target-densityDpi=device-dpi"/>
-    <title>
-        <?php
-        echo tpl_pagetitle($ID, true) . (preg_match('/výfuk/i', tpl_pagetitle($ID, true)) ?: " - " . strip_tags($conf['title']))
-		//echo tpl_pagetitle($ID, true)  . ' - ' . strip_tags($conf['title'])
-        ?>
-
-    </title>
+	<!--Files important for Bootstrap-->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
+	integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" 
+	integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+	integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" 
+	integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	
+	<meta charset="utf-8"/>
+	<title><?php
+	if($ID == "start"){
+		//echo "Vítejte na stránkách Výfuku!";
+		echo $pageId;
+	}
+	else{
+		tpl_pagetitle(); echo " - Výfuk";
+	}
+	?>
+	</title>
     <?php
-    tpl_metaheaders();
     echo tpl_favicon(['favicon', 'mobile']);
+	tpl_metaheaders();
     ?>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"
-            integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb"
-            crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"
-            integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn"
-            crossorigin="anonymous"></script>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
-    <meta name="google-site-verification" content="Z__tiJ79t7gWSoTBODkD_FP3YXja-tL4myNJSh5JFKc"/>
 </head>
-<body data-act="<?php echo $ACT; ?>"
-      data-namespace="<?php echo getNS($ID); ?>"
-      data-page_id="<?php echo $ID; ?>">
 
-<div class="dokuwiki" id="dokuwiki" data-do="<?php echo $ACT ?>">
-    <div class="bg-vyfuk-green">
-        <?php
-        $fullMenu = new \fksTemplate\NavBar\BootstrapNavBar('full');
-        $fullMenu->setClassName('container navbar-inverse')
-            ->addMenuText('menu')
-			->addBrand('', null, 'images/logo_w.png', 55, null)
-            ->addTools('ml-auto justify-content-end', true)
-            ->render();
-        ?>
-    </div>
-    <div class="container-fluid main-container" style="max-width: 1280px">
-        <header class="header" align="center">
-            <div class="row justify-content-around pt-3 pb-3">
-                <div class="col-lg-4 col-md-8 image-fluid justify-content-center">
-                    <a
-                            href="<?php echo DOKU_BASE ?>">
-                        <img
-                                src="<?php echo DOKU_TPL . '/images/logo.svg'; ?>"
-                                alt="logo_vyfuk"/>
-                    </a>
-                </div>
-
-                <div class="col-lg-2 hidden-md-down header-image-1">
-                    <img
-                            src="<?php echo DOKU_TPL . '/images/header-1.png'; ?>"
-                            alt="logo_vyfuk"
-                    />
-                </div>
-                <div class="col-lg-2 hidden-md-down header-image-2">
-                    <img
-                            src="<?php echo DOKU_TPL . '/images/header-2.png'; ?>"
-                            alt="logo_vyfuk"
-                    />
-                </div>
-                <div class="col-lg-3 hidden-md-down header-image-3">
-                    <img
-                            src="<?php echo DOKU_TPL . '/images/header-3.png'; ?>"
-                            alt="logo_vyfuk"
-                    />
-                </div>
-            </div>
-        </header>
-        <?php
-        flush();
-        ?>
-        <div class="row">
-            <main class="col-lg-8 col-md-12">
-                <?php
-                tpl_content();
-                ?>
-            </main>
-            <?php
-            flush();
-            if ($ACT === 'show') {
-                ?>
-                <aside class="col-lg-4 col-md-12">
-                    <?php tpl_include_page('system:fkssidebar'); ?>
-                </aside>
-                <?php
-            }
-            tpl_flush();
-            ?>
-            <div class="no">
-                <?php
-                tpl_indexerWebBug();
-                ?>
-            </div>
-        </div>
-        <footer class="text-center pt-3 mt-3">
-            <?php echo p_render('xhtml', p_cached_instructions('data/pages/system/fksfootbar.txt'), $info); ?>
-            <div class="row">
-                <a class="col-6"
-                   href="http://wiki.splitbrain.org/wiki:dokuwiki" title="Driven by DokuWiki">
-                    <img src="<?php echo DOKU_TPL . 'images/button-dw.png'; ?>" width="80" height="15"
-                         alt="Driven by DokuWiki"/>
-                </a>
-                <a class="col-6"
-                   href="<?php echo DOKU_BASE . 'feed.php'; ?>" title="Recent changes RSS feed">
-                    <img src="<?php echo DOKU_TPL . 'images/button-rss.png'; ?>" width="80" height="15"
-                         alt="Recent changes RSS feed"/>
-                </a>
-            </div>
-        </footer>
-
-    </div>
-
+<body data-act="<?php echo $ACT; ?>" data-namespace="<?php echo getNS($ID); ?>" data-page_id="<?php echo $ID; ?>">
+	<div class="dokuwiki" id="dokuwiki" data-do="<?php echo $ACT ?>" >
+		<div class="component" id="menuBackground">
+			<nav class="navbar navbar-expand-lg sticky-top navbar-dark">
+				<?php
+				$fullMenu = new \fksTemplate\NavBar\BootstrapNavBar('full');
+				$fullMenu->setClassName('container')
+					->addMenuText('menu')
+					->addBrand('', '', 'images/logo-w.svg', 50, null)
+					->addTools('ml-auto justify-content-end', true)
+					->render();
+				?>
+			</div>
+		</nav>
+		<?php if ($pageId == "start"): ?>
+			<div id="mainSlider" class="container-fluid carousel slide" data-ride="carousel" style="text-align: center">
+	<ol class="carousel-indicators">
+		<li data-target="#mainSlider" data-slide-to="0" class="active"></li>
+		<li data-target="#mainSlider" data-slide-to="1"></li>
+		<li data-target="#mainSlider" data-slide-to="2"></li>
+	</ol>
+	<div class="carousel-inner">
+		<div class="carousel-item active" data-interval="12000">
+			<img src="/lib/tpl/vyfuk/images/slider1.png" class="d-block w-100">
+			<div class="carousel-caption">
+				<h5>Kdo jsme?</h5>
+				<p>Výfuk je fyzikální soutěž pro základoškoláky a nižší gymnázia. S námi zažiješ fyziku v novém rozměru!</p>
+			</div>
+		</div>
+		<div class="carousel-item" data-interval="10000">
+			<img src="/lib/tpl/vyfuk/images/slider2.png" class="d-block w-100">
+			<div class="carousel-caption">
+				<h5>Výpočty i zábava</h5>
+				<p>I u fyziky se můžeš pobavit. Zkus si vlastní pokus nebo s námi vyjeď na zajímavá místa po celé republice!</p>
+			</div>
+		</div>
+		<div class="carousel-item" data-interval="10000">
+			<img src="/lib/tpl/vyfuk/images/slider3.png" class="d-block w-100">
+			<div class="carousel-caption">
+				<h5>Přidej se k nám!</h5>
+				<p>Můžeš začít řešit hned, stačí se zaregistrovat a poslat nám svou práci.</p>
+			</div>
+		</div>
+	</div>
 </div>
+		<?php endif; ?>
+		<div class="container-fluid main-container" style="max-width: 1000px">
+			<?php tpl_content(); ?>
+		</div>
+	</div>
 </body>
-</html>
-
-
-
